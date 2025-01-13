e9917a62f55ff1689109c5c4b410461c76344cf4
vbisso <valeriabisso19@gmail.com>
2025-01-13 16:01:44 -0700

    more route handlers

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const name = process.env.NAME; //process = global object. It takes the name from
 
 // Define a route handler for the root URL ('/')
 app.get("/", (req, res) => {
-  res.send(`Hello, ${name}! How are you?`);
+  res.send(`Hello, ${name}!`);
+});
+
+app.get("/about", (req, res) => {
+  res.send(
+    "I was born in February 22th so I'm a piscis! :) I love to read, eat and sleep! I'm currently watching Naruto for the first time! My favorite characters at the moment are Naruto, Kakashi, Hinata and Neji <3"
+  );
 });
 
 // Define the port number the server will listen on
